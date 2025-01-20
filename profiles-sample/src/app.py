5983049871978a43ab9286f76d55ccbd55d1eef4
Xiao Chen <57294625+ouerxiao@users.noreply.github.com>
2025-01-20 00:50:23 -0500

    Add files via upload

---
+++ profiles-sample/src/app.py
@@ -0,0 +1,11 @@
+import sys
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def about():
+    return render_template('about.html')
+
+if __name__ == "__main__":
+        app.run()
